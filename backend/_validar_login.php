53ae63c19cac0e5cedf6b2823df6c3871eead0ea
luana maldonado <51408179+luanadacosta@users.noreply.github.com>
2022-08-17 20:09:32 -0300

    senha case sensitive add

--- backend/_validar_login.php
+++ backend/_validar_login.php
@@ -1,24 +1,41 @@
 <?php
-
-include "conexao";
+//include inclui a conexao
+include "conexao.php";
 
 try{
 
     $usuario =$_POST['usuario'];
     $senha =$_POST['senha'];
 
-$sql = "SELECT * FROM tb_login WHERE usuario = '$usuario' AND senha = '$senha'";
+$sql = "SELECT * FROM tb_login WHERE email = '$usuario' AND BINARY senha = '$senha' AND ativo = 1";
 
 $comando = $con->prepare($sql);
 
-$comando = $execute();
+$comando->execute();
 
 $dados = $comando->fetchAll(PDO::FETCH_ASSOC);
 
-var_dump($dados);
+//var_dump($dados);
 
-}catch(PDOException $erro){
-    echo $erro->getMessage();
+//verifica se existem registros dentro da variavel dados
+//if se email senha e ativo 1 da certo, si não ja vai em else
+if($dados != null){
+    header('Location:../admin/gerenciar_viagens.php');
+}else{
+    //se o usuario e senha nao sao validos, ira entrar nesse bloco de codigo
+    echo "Usuario ou senha invalidos";
+//header('Location:../admin/index.html');
 }
 
-?>
+
+
+
+
+
+}catch(PDOException $erro){
+//se o ususario e senha sao validos, ira entrar nesse bloco de codigo
+
+
+    echo $erro->getMessage();
+
+}
